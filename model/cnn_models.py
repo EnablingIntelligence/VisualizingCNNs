7d131d51068162d3ba9a61cf9dc1d22981ebd777
Sesko <Sesko.DK@gmail.com>
2024-01-21 20:57:02 +0100

    implemented second version of alexnet

--- model/cnn_models.py
+++ model/cnn_models.py
@@ -3,7 +3,62 @@ import torch.nn as nn
 from normalizer import Normalizer
 
 
-class AlexNet(nn.Module):
+class AlexNet1(nn.Module):
+    """
+    This implementation is based on section 4.1:
+    https://arxiv.org/pdf/1311.2901.pdf
+
+    By default we use the local response normalization.
+
+    Input: Tensor-Images with the shape B x 3 x 224 x 224 (B x C x H x W).
+    Output: Tensor with B x num_classes, where B represents the batch size.
+    """
+
+    def __init__(
+        self,
+        in_channels: int = 3,
+        num_classes: int = 1_000,
+        dropout: float = 0.5,
+    ):
+        super().__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels, 96, kernel_size=7, stride=2, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            Normalizer(),
+            nn.Conv2d(96, 256, kernel_size=5, stride=2, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            Normalizer(),
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            Normalizer(),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=dropout),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+
+class AlexNet2(nn.Module):
     """
     This implementation is based on section 3.5:
     https://proceedings.neurips.cc/paper_files/paper/2012/file/c399862d3b9d6b76c8436e924a68c45b-Paper.pdf
@@ -58,7 +113,9 @@ class AlexNet(nn.Module):
 
 
 if __name__ == "__main__":
-    m = AlexNet()
+    m = AlexNet1()
+    # m = AlexNet2()
+    
     B, C, H, W = 4, 3, 224, 224
     batch = torch.randn(size=(B, C, H, W), dtype=torch.float32)
     output = m(batch)
