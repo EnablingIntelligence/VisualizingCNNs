70a93da33c85ea86ebf280ac8eed2b2481d84c55
Sesko <Sesko.DK@gmail.com>
2024-01-21 17:13:53 +0100

    add doc string in normalizer class

--- model/normalizer.py
+++ model/normalizer.py
@@ -12,8 +12,7 @@ class ContrastNorm(nn.Module):
     """
     https://proceedings.neurips.cc/paper_files/paper/2012/file/c399862d3b9d6b76c8436e924a68c45b-Paper.pdf
     """
-
-    def __init__(self) -> None:
+    def __init__(self):
         super().__init__()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -25,8 +24,7 @@ class LocalResponseNorm(nn.Module):
     """
     https://pytorch.org/docs/stable/generated/torch.nn.LocalResponseNorm.html#torch.nn.LocalResponseNorm
     """
-
-    def __init__(self, size: int = 2) -> None:
+    def __init__(self, size: int = 2):
         super().__init__()
         self.local_norm = nn.LocalResponseNorm(size)
 
@@ -35,7 +33,14 @@ class LocalResponseNorm(nn.Module):
 
 
 class Normalizer(nn.Module):
-    def __init__(self, normalization_method: Norm = Norm.CONTRAST, local_size: int = 2) -> None:
+    """
+    This module enables two normalization methods.
+     - Contrast normalization
+     - Local response normalization 
+    """
+    def __init__(
+        self, normalization_method: Norm = Norm.CONTRAST, local_size: int = 2
+    ):
         super().__init__()
         if normalization_method == Norm.CONTRAST:
             self.model = ContrastNorm()
